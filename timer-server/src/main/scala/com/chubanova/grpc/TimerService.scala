736089400801e0aa35edc9f7f3a2f2a137b65376
maria <m.a.chubanova@gmail.com>
2020-05-21 16:43:51 +0300

    add startTime Functional with return

--- timer-server/src/main/scala/com/chubanova/grpc/TimerService.scala
+++ timer-server/src/main/scala/com/chubanova/grpc/TimerService.scala
@@ -1,12 +1,13 @@
 package com.chubanova.grpc
 
 import com.chubanova.interactor.TimerInteractor
+import com.chubanova.model.TimeForProject
 import io.grpc.Status
 import io.grpc.stub.StreamObserver
 import org.lognet.springboot.grpc.GRpcService
 import org.springframework.beans.factory.annotation.Autowired
-import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.collection.JavaConverters._
 import scala.util.{Failure, Success}
 
@@ -22,16 +23,7 @@ class TimerService(@Autowired val timerInteractor: TimerInteractor = null) exten
         responseObserver.onNext(Response.newBuilder().build())
         responseObserver.onCompleted()
       case Success(x) =>
-        responseObserver.onNext(
-          Response
-            .newBuilder()
-            .setAllTime(x.map(t => t.time).sum)
-            .setProject(Project
-              .newBuilder()
-              .setName(project)
-              .setSubProject(subproject)
-              .build())
-            .build())
+        generateResponse(responseObserver, project, subproject, x)
         responseObserver.onCompleted()
       case Failure(exception) =>
         responseObserver.onError(Status.INTERNAL.withDescription(exception.getMessage).asException())
@@ -47,7 +39,7 @@ class TimerService(@Autowired val timerInteractor: TimerInteractor = null) exten
         responseObserver.onNext(Response.newBuilder().build())
         responseObserver.onCompleted()
       case Success(x) =>
-        responseObserver.onNext(Response.newBuilder().build())
+        generateResponse(responseObserver, project, subproject, x)
         responseObserver.onCompleted()
       case Failure(exception) =>
         responseObserver.onError(Status.INTERNAL.withDescription(exception.getMessage).asException())
@@ -70,5 +62,23 @@ class TimerService(@Autowired val timerInteractor: TimerInteractor = null) exten
     //      case Failure(exception) =>
     //        responseObserver.onError(Status.INTERNAL.withDescription(exception.getMessage).asException())
     //    }
+  }
+
+  private def generateResponse(responseObserver: StreamObserver[Response], project: String, subproject: String, x: Seq[TimeForProject]) = {
+    val time = x.map(t => t.time).sum
+    val minutes = ((time) % 60).asInstanceOf[Int]
+    val hours = ((time / (60)) % 24).asInstanceOf[Int]
+    val days = ((time / (60 * 24)) % 7).asInstanceOf[Int]
+    val week = (time / (60 * 24 * 7)).asInstanceOf[Int]
+    responseObserver.onNext(Response
+      .newBuilder()
+      .setStatus(f"$week%d week $days%d days $hours%d hours $minutes%d minutes")
+      .setAllTime(time)
+      .setProject(Project
+        .newBuilder()
+        .setName(project)
+        .setSubProject(subproject)
+        .build())
+      .build())
   }
 }
--- timer-server/src/main/scala/com/chubanova/repository/TimerRepository.scala
+++ timer-server/src/main/scala/com/chubanova/repository/TimerRepository.scala
@@ -1,5 +1,7 @@
 package com.chubanova.repository
 
+import java.time.{LocalDateTime, Period}
+import java.time.temporal.ChronoUnit
 import java.util.Date
 
 import com.chubanova.grpc.WatchStatisticFilter
@@ -9,11 +11,13 @@ import org.mongodb.scala.MongoDatabase
 import org.mongodb.scala.model.Filters._
 
 import scala.concurrent.ExecutionContext
+import scala.util.Success
 
 class TimerRepository(val mongoDatabase: MongoDatabase) {
   /**
-   *  Needs to insert, update objects to Mongo
+   * Needs to insert, update objects to Mongo
    */
+
   import org.bson.codecs.configuration.CodecRegistries._
   import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
   import org.mongodb.scala.bson.codecs.Macros._
@@ -22,27 +26,32 @@ class TimerRepository(val mongoDatabase: MongoDatabase) {
 
   private val codecRegistry = fromRegistries(customCodecs, DEFAULT_CODEC_REGISTRY)
 
-  val spendTimes = mongoDatabase.getCollection[TimeForProject]("spendTimes")// this need to insert scala objects not BSON Document
+  val spendTimes = mongoDatabase.getCollection[TimeForProject]("spendTimes") // this need to insert scala objects not BSON Document
     .withCodecRegistry(codecRegistry)
-  val timer = mongoDatabase.getCollection[Timer]("timer")// this need to insert scala objects not BSON Document
+  val timer = mongoDatabase.getCollection[Timer]("timer") // this need to insert scala objects not BSON Document
     .withCodecRegistry(codecRegistry)
 
-  def addTime(times: Long, project: String, subproject: String)(implicit executionContext: ExecutionContext) ={
-    val timesData = TimeForProject(project,subproject,times, new Date())
+  def addTime(times: Long, project: String, subproject: String)(implicit executionContext: ExecutionContext) = {
+    val timesData = TimeForProject(project, subproject, times, new Date())
     spendTimes.insertOne(timesData).toFuture()
 
     findAllTimesByProject(project, subproject)
   }
 
 
-  def startTimer(start: Int, project: String, subproject: String)(implicit executionContext: ExecutionContext) ={
-    val timesData = Timer(project,subproject,new Date())
-    if(start==0){
+  def startTimer(start: Int, project: String, subproject: String)(implicit executionContext: ExecutionContext) = {
+    val timesData = Timer(project, subproject, new Date())
+    if (start == 0) {
       timer.insertOne(timesData).toFuture()
-    } else{
-      var temp : Date = new Date()
-      timer.find(and(equal("project", project), equal("subProject", subproject))).toFuture().map(t=>t.foreach(it=>temp =it.updated))
-      println(temp)
+    } else {
+      timer.find(and(equal("project", project), equal("subProject", subproject))).head().onComplete {
+        case Success(x) =>
+
+          val minutes = (new Date().getTime - x.updated.getTime) / (1000 * 60).asInstanceOf[Long]
+          val timesData = TimeForProject(project, subproject, minutes, new Date())
+          spendTimes.insertOne(timesData).toFuture()
+          timer.deleteOne(and(equal("project", project), equal("subProject", subproject))).toFuture()
+      }
 
     }
 
@@ -53,10 +62,10 @@ class TimerRepository(val mongoDatabase: MongoDatabase) {
   def watchStatistic(period: Int, project: Int, projectList: Array[WatchStatisticFilter.Project], from: Timestamp, to: Timestamp) = ???
 
 
-  private def findAllTimesByProject(project: String, subproject: String)={
+  private def findAllTimesByProject(project: String, subproject: String) = {
 
-//    spendTimes.aggregate(Seq(Aggregates.group("$project",
-//      Accumulators.sum("times", 1)))).toFuture() //TODO Make normal sum
+    //    spendTimes.aggregate(Seq(Aggregates.group("$project",
+    //      Accumulators.sum("times", 1)))).toFuture() //TODO Make normal sum
     spendTimes.find(and(equal("project", project), equal("subProject", subproject))).toFuture()
 
   }
